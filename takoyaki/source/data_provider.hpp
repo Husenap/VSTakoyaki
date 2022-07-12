0c15ca0a991507be1f5ef2fb85512c4d12db5f93
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-07-12 18:08:21 +0200

    added data provider class

---
+++ takoyaki/source/data_provider.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <atomic>
+
+namespace ht {
+
+template <typename T>
+class DataProvider {
+public:
+    void request() { mFlag = false; }
+    void serve() { mFlag = true; }
+
+    T&       data() { return mData; }
+    T const& data() const { return mData; }
+
+    T& operator*() { return mData; }
+    T const& operator*() const { return mData; }
+
+    operator bool() const { return mFlag; }
+
+private:
+    std::atomic_bool mFlag{false};
+    T                mData{};
+};
+
+}  // namespace ht
